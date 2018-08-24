6c6ba6d7b08cac6f10e8eed98c1a0f440bd14a2c
Michael Fuller <fullerm@tmacog.org>
2018-08-24 15:38:56 -0400

    refactor with tidycensus;add scale bar; filter michigan block groups;

--- pop2010.R
+++ pop2010.R
@@ -1,6 +1,8 @@
 library(tidycensus)
 library(tidyverse)
 library(censusapi)
+library(tigris)
+library(tmap)
 #census_api_key('b7da053b9e664586b9e559dba9e73780602f0aab')
 key =Sys.getenv("CENSUS_API_KEY")
 vintage = 2010
@@ -9,50 +11,49 @@ name = "sf1"
 region = "block group:*"
 
 
-luc2010pop <- getCensus(name = name,
-                        vintage = vintage,
-                        key = key,
-                        vars=variable,
-                        region= region,
-                        regionin = "state:39+county:095")
+luc2010pop <- get_decennial(geography = "block group",
+                            variables = variable,
+                            year = vintage,
+                            state = "39",
+                            county = "095",
+                            key = key,
+                            geometry = TRUE)
 
-woo2010pop <- getCensus(name = name,
-                        vintage = vintage,
-                        key = key,
-                        vars=variable,
-                        region= region,
-                        regionin = "state:39+county:173")
+woo2010pop <- get_decennial(geography = "block group",
+                            variables = variable,
+                            year = vintage,
+                            state = "39",
+                            county = "173",
+                            key = key,
+                            geometry = TRUE)
 
-mon2010pop <- getCensus(name = name,
-                        vintage = vintage,
-                        key = key,
-                        vars=variable,
-                        region= region,
-                        regionin = "state:26+county:115")
+mon2010pop <- get_decennial(geography = "block group",
+                            variables = variable,
+                            year = vintage,
+                            state = "26",
+                            county = "115",
+                            key = key,
+                            geometry = TRUE)
+mon2010pop <- mon2010pop %>% 
+  filter(grepl("26115833", GEOID, fixed = TRUE)) %>%
+  spread(variable, value)
+luc2010pop <- luc2010pop %>%
+  spread(variable,value)
+woo2010pop <- woo2010pop %>%
+  spread(variable,value)
 
 tmacog2010pop <- rbind(luc2010pop,woo2010pop,mon2010pop)
 
 
 tmacog2010features <- rbind_tigris(block_groups("OH",county = c("Lucas","Wood"),year = 2010,cb = TRUE),
-                                   block_groups("MI",county = "Monroe", cb = TRUE,year = 2010))
-#merge(tmacog1990features,tmacog1990pop)
-tmacog2010pop$geoid = paste(tmacog2010pop$state,
-                            tmacog2010pop$county,
-                            tmacog2010pop$tract,
-                            tmacog2010pop$block.group,
-                            sep = "")
-tmacog2010features@data$GEOID = paste(tmacog2010features@data$STATE,
-                                      tmacog2010features@data$COUNTY,
-                                      tmacog2010features@data$TRACT,
-                                      tmacog2010features@data$BLKGROUP,
-                                      sep = "")
+                                   mon2010features
+                                   )
 
-tmacog_joined2010 <- geo_join(tmacog2010features,tmacog2010pop,"GEOID","geoid")
-#plot(block_groups("OH",county = c("Lucas","Wood"),year = 1990,cb = TRUE))
-tm_shape(tmacog_joined) +tm_fill("P0010001",style = "quantile", n = 5, palette = "Purples")+
+tm_shape(tmacog2010pop) +tm_fill("P0010001",style = "quantile", n = 5, palette = "Purples",title = "Population")+
   tm_layout(bg.color = "ivory",
             title = "TMACOG Region Population Density 2010\nby Census Block Group",
             title.position = c("center","top"), title.size = 1.1,
             legend.position = c(0.8,0), legend.text.size = 0.75,
             legend.width = 0.2)+
+  tm_scale_bar()+
   tm_credits(("Data source: US Census Bureau"))
